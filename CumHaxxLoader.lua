5d914771ff4838158fd6f120a89382fa197e7089
RobloxScriptBackups <85185411+RobloxScriptBackups@users.noreply.github.com>
2022-06-13 17:16:04 -0400

    Update CumHaxxLoader.lua

--- CumHaxxLoader.lua
+++ CumHaxxLoader.lua
@@ -1,7 +1,7 @@
 if game.GameId == 3233893879 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/ascaxz/ReleaseScripts/main/CumHaxxBB.lua")()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxScriptBackups/CumHaxxBackup/main/CumHaxxBadBusiness.lua")()
 elseif game.GameId == 2377868063 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/ascaxz/ReleaseScripts/main/CumHaxxStrucid.lua")()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxScriptBackups/CumHaxxBackup/main/CumHaxxStrucid.lua")()
 else
     Kick("CumHaxx does not work on this game. It only works on Strucid and Bad Business.")
 end
